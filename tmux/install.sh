ecc1059188e49b6e182ac937c1e69434ec5bbcd0
Andriy Kulynyak <kulynyak@gmail.com>
2022-09-03 22:49:08 +0300

    Update tmux setup

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-ln -sfn $PWD/tmux.conf $HOME/.tmux.conf
-mkdir -p $PWD/tmux
-ln -sfn $PWD/tmux $HOME/.tmux
-mkdir -p $HOME/.tmux/plugins
+_OS=$(uname)
+
+case $_OS in
+Darwin)
+  brew install tmux
+  ;;
+*)
+  sudo dnf install tmux
+  ;;
+esac
+
+ln -sfn "$PWD/tmux.conf" "$HOME/.tmux.conf"
+mkdir -p "$PWD/tmux"
+ln -sfn "$PWD/tmux" "$HOME/.tmux"
+mkdir -p "$HOME/.tmux/plugins"
+cd "$HOME/.tmux/plugins"
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm
 fi
