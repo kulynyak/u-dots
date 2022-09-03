e84d01efefc1862d96913470bbfcad6e98fa8750
Andriy Kulynyak <kulynyak@gmail.com>
2022-09-03 22:56:30 +0300

    Update alakritty setup

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-if [ "x$__OS" != "xDarwin" ]; then
-  exit
+# Darwin only
+
+_OS=$(uname)
+if test "$_OS" = "Darwin"; then
+  brew install rust
+
+  ln -sfn "$PWD/alacritty" "$HOME/.config/alacritty"
+
+  al_tmp="$HOME/tmp"
+  if [ ! -d "$al_tmp/alacritty" ]; then
+    mkdir -p "$al_tmp"
+    cd "$al_tmp"
+    git clone https://github.com/alacritty/alacritty.git
+    cd alacritty
+    make app && cp -rf target/release/osx/Alacritty.app /Applications/
+  fi
 fi
-
-brew install rust
-ln -sfn $PWD/alacritty $HOME/.config/alacritty
-
-al_tmp=$HOME/tmp 
-if [ ! -d "$al_tmp/alacritty" ]; then
-  mkdir -p $al_tmp
-  cd $al_tmp
-  git clone https://github.com/alacritty/alacritty.git 
-  cd alacritty
-  make app && cp -rf target/release/osx/Alacritty.app /Applications/
-fi
-
-# brew install node
-# brew install npm
-# npm i -g alacritty-themes
