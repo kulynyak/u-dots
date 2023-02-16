fdc40449136a2f3e2da2b019f68f8398c0bd1157
Andriy Kulynyak <kulynyak@gmail.com>
2023-02-16 10:43:53 +0200

    update services instal script for fedora

--- fedora/services.sh
+++ fedora/services.sh
@@ -1,8 +1,8 @@
 # ssh
 sudo firewall-cmd --zone=public --permanent --add-service=ssh
 sudo firewall-cmd --reload
+sudo systemctl daemon-reload
 sudo systemctl enable sshd
 sudo systemctl start sshd
-sudo systemctl status sshd
 
 # other
