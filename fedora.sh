f1eebd73e4125ad321c71458817a8010dc0af74f
Andriy Kulynyak <kulynyak@gmail.com>
2023-02-16 09:45:05 +0200

    fix for setup scripts

--- fedora.sh
+++ fedora.sh
@@ -2,10 +2,10 @@
 
 inst_items=('fedora' 'git' 'tmux' 'zsh')
 u_dots=$PWD
-for i in $inst_items; do
+for i in "${inst_items[@]}"; do
   echo "installing $i ..."
-  cd $i
+  cd "$i"
   ./install.sh
   echo "$i installed"
-  cd $u_dots
+  cd "$u_dots"
 done
--- macos.sh
+++ macos.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Homebrew (if not installed)
-if test ! $(which brew); then
+if test ! "$(which brew)"; then
   echo "Installing Homebrew for you."
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"; then
@@ -11,23 +11,23 @@ fi
 
 inst_items=('alacritty' 'git' 'karabiner' 'tmux' 'zsh' 'wezterm' 'kitty')
 u_dots=$PWD
-for i in $inst_items; do
+for i in "${inst_items[@]}"; do
   echo "installing $i ..."
-  cd $i
+  cd "$i"
   ./install.sh
   echo "$i installed"
-  cd $u_dots
+  cd "$u_dots"
 done
 echo "installing hammerspoon ..."
 hammerspoon=$HOME/.hammerspoon
 if [ ! -d "$hammerspoon" ]; then
-  git clone git@github.com:kulynyak/hammerspoon.git $hammerspoon
-  ln -s $hammerspoon $u_dots/hammerspoon
+  git clone git@github.com:kulynyak/hammerspoon.git "$hammerspoon"
+  ln -s "$hammerspoon" "$u_dots/hammerspoon"
 fi
 cd hammerspoon
 ./install.sh
 echo "hammerspoon installed"
-cd $u_dots
+cd "$u_dots"
 echo "installing nvim ..."
 nvim=$u_dots/nvim
 if [ ! -d "$nvim" ]; then
@@ -36,7 +36,7 @@ fi
 cd nvim
 /.install.sh
 echo "nvim installed"
-cd $u_dots
+cd "$u_dots"
 echo "installing Brewfile ..."
-brew bundle --file=$u_dots/brew/Brewfile
+brew bundle --file="$u_dots/brew/Brewfile"
 echo "Brewfile installed"
