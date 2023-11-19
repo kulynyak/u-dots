921964cc1b853697047f845a66056e7143e361cb
Andriy Kulynyak <ra997716635@gmail.com>
2023-11-19 22:41:05 +0200

    zsh: nvim related changes

--- zsh/zshrc.d/nvims.sh
+++ zsh/zshrc.d/nvims.sh
@@ -3,9 +3,10 @@
 alias vl="NVIM_APPNAME=LazyVim nvim"
 alias vc="NVIM_APPNAME=NvChad nvim"
 alias va="NVIM_APPNAME=AstroNvim nvim"
+alias vp="NVIM_APPNAME=NvPunk nvim"
 
 function nvims() {
-  items=("default" "LazyVim" "NvChad" "AstroNvim")
+  items=("default" "LazyVim" "NvChad" "AstroNvim" "NvPunk")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config » " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
@@ -18,4 +19,33 @@ function nvims() {
 
 bindkey -s ^v "nvims\n"
 
+
+
+function drop_vim() {
+    local name=$1
+    # Exit if name is empty or contains only spaces
+    if [[ -z "${name// /}" ]]; then
+        echo "No name provided. Exiting."
+        return
+    fi
+    # Get current date and time in the specified format
+    local datetime=$(date "+%Y-%m-%d-%H-%M-%S")
+
+    # Check and remove directories, print status
+    for dir in ~/.local/share/$name ~/.local/state/$name ~/.cache/$name; do
+        if [[ -d $dir ]]; then
+            rm -rf $dir
+            echo "$dir deleted."
+        else
+            echo "$dir does not exist."
+        fi
+    done
+    # Rename directory in ~/.config if it exists
+    if [[ -d ~/.config/$name ]]; then
+        mv ~/.config/$name ~/.config/${name}-${datetime}
+        echo "~/.config/$name renamed to ~/.config/${name}-${datetime}."
+    else
+        echo "~/.config/$name does not exist."
+    fi
+}
 
