cd9dee571af5eafec80980691327fdb4041aa7c3
Andriy Kulynyak <ra997716635@gmail.com>
2023-11-21 23:39:21 +0200

    astronvim: initial commit

---
+++ AstroNvim/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.config/AstroNvim
+ln -sfn "$PWD"/user "$HOME/.config/AstroNvim/user"
---
+++ AstroNvim/user/highlights/duskfox.lua
@@ -0,0 +1,6 @@
+-- stylua: ignore
+if true then return {} end
+
+return { -- a table of overrides/changes to the duskfox theme
+  Normal = { bg = "#000000" },
+}
