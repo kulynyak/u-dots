63edd0a1413412c1c0dc2c092948592f116971b6
Andrii Kulyniak <ra997716635@gmail.com>
2025-02-14 09:44:38 +0200

    update android.sh to set ANDROID_SDK_ROOT and adjust PATH for command line tools

--- zsh/zshrc.d/android.sh
+++ zsh/zshrc.d/android.sh
@@ -1,9 +1,23 @@
 #!/bin/zsh
 
+### install macos
+#
+# brew install android-studio
+# brew install --cask temurin
+# brew install --cask android-commandlinetools
+# sdkmanager "platform-tools" "platforms;android-34" "build-tools;34.0.0"
+###
+
 # Set ANDROID_HOME based on the operating system
 case "$__OS" in
 Darwin)
-  ANDROID_HOME="$HOME/Library/Android/sdk"
+  # ANDROID_SDK_ROOT=$(brew info --cask android-commandlinetools | awk -F 'Default Android SDK root is' '{if ($2 != "") print $2}' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+  ANDROID_SDK_ROOT=/opt/homebrew/share/android-commandlinetools
+  if [[ -d $ANDROID_SDK_ROOT ]]; then
+    export ANDROID_SDK_ROOT
+    export ANDROID_HOME=$ANDROID_SDK_ROOT
+    export PATH="$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/emulator:$PATH"
+  fi
   ;;
 Fedora)
   # Optionally set ANDROID_HOME for Fedora or leave it empty
@@ -14,9 +28,3 @@ Fedora)
   ANDROID_HOME=""
   ;;
 esac
-
-# If ANDROID_HOME is set and the directory exists, update the PATH
-[[ -d $ANDROID_HOME ]] && {
-  export ANDROID_HOME
-  PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools/bin:$PATH"
-}
